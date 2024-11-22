cff23d357dea081de8be29489779ff7f42bae647
Petr Cerveny <cervenyxp@gmail.com>
2024-11-22 21:19:39 +0100

    xx

--- custom_components/sscp_integration/sensor.py
+++ custom_components/sscp_integration/sensor.py
@@ -1,29 +1,49 @@
 import logging
-from homeassistant.components.sensor import SensorEntity
-from homeassistant.helpers.update_coordinator import CoordinatorEntity
+from homeassistant.components.sensor import SensorEntity, SensorDeviceClass, SensorStateClass
+from homeassistant.const import UnitOfTemperature, UnitOfPressure, UnitOfLength, UnitOfVoltage, UnitOfEnergy, UnitOfPower
 from . import DOMAIN
 
 _LOGGER = logging.getLogger(__name__)
 
-# Mapování jednotek na device_class
+# Mapování jednotek na device_class a state_class
 UNIT_DEVICE_CLASS_MAP = {
-    "°C": "temperature",
-    "°F": "temperature",
-    "Pa": "pressure",
-    "kPa": "pressure",
-    "bar": "pressure",
-    "m": "distance",
-    "cm": "distance",
-    "mm": "distance",
-    "V": "voltage",
-    "mV": "voltage",
-    "A": "current",
-    "mA": "current",
-    "Hz": "frequency",
-    "W": "power",
-    "kW": "power",
-    "kWh": "energy",
-    "%": "humidity",
+    "°C": SensorDeviceClass.TEMPERATURE,
+    "°F": SensorDeviceClass.TEMPERATURE,
+    "Pa": SensorDeviceClass.PRESSURE,
+    "kPa": SensorDeviceClass.PRESSURE,
+    "bar": SensorDeviceClass.PRESSURE,
+    "m": SensorDeviceClass.DISTANCE,
+    "cm": SensorDeviceClass.DISTANCE,
+    "mm": SensorDeviceClass.DISTANCE,
+    "V": SensorDeviceClass.VOLTAGE,
+    "mV": SensorDeviceClass.VOLTAGE,
+    "A": SensorDeviceClass.CURRENT,
+    "mA": SensorDeviceClass.CURRENT,
+    "Hz": SensorDeviceClass.FREQUENCY,
+    "W": SensorDeviceClass.POWER,
+    "kW": SensorDeviceClass.POWER,
+    "kWh": SensorDeviceClass.ENERGY,
+    "%": SensorDeviceClass.HUMIDITY,
+}
+
+UNIT_STATE_CLASS_MAP = {
+    "°C": SensorStateClass.MEASUREMENT,
+    "°F": SensorStateClass.MEASUREMENT,
+    "Pa": SensorStateClass.MEASUREMENT,
+    "kPa": SensorStateClass.MEASUREMENT,
+    "bar": SensorStateClass.MEASUREMENT,
+    "m": SensorStateClass.MEASUREMENT,
+    "cm": SensorStateClass.MEASUREMENT,
+    "mm": SensorStateClass.MEASUREMENT,
+    "V": SensorStateClass.MEASUREMENT,
+    "mV": SensorStateClass.MEASUREMENT,
+    "A": SensorStateClass.MEASUREMENT,
+    "mA": SensorStateClass.MEASUREMENT,
+    "Hz": SensorStateClass.MEASUREMENT,
+    "W": SensorStateClass.MEASUREMENT,
+    "kW": SensorStateClass.MEASUREMENT,
+    "kWh": SensorStateClass.MEASUREMENT,
+    "%": SensorStateClass.MEASUREMENT,
 }
 
 async def async_setup_entry(hass, config_entry, async_add_entities):
@@ -42,47 +62,30 @@ async def async_setup_entry(hass, config_entry, async_add_entities):
     if sensors:
         async_add_entities(sensors, update_before_add=True)
 
-class SSCPVariableSensor(CoordinatorEntity, SensorEntity):
+class SSCPVariableSensor(SensorEntity):
     """Reprezentace SSCP senzoru."""
 
     def __init__(self, client, config, entry_id):
         """Inicializace senzoru."""
-        super().__init__(client)
         self._client = client
         self._uid = config["uid"]
         self._offset = config.get("offset", 0)
         self._length = config.get("length", 1)
         self._type = config["type"]
-        self._name = config["name"]
-        self._unit_of_measurement = config.get("unit_of_measurement", None)
-        self._device_class = config.get("device_class", UNIT_DEVICE_CLASS_MAP.get(self._unit_of_measurement))
-        self._state = None
         self._entry_id = entry_id
 
-    @property
-    def name(self):
-        """Vrátí název senzoru."""
-        return self._name
+        # Atributy nastavené dynamicky podle konfigurace
+        self._attr_name = config["name"]
+        self._attr_native_unit_of_measurement = config.get("unit_of_measurement")
+        self._attr_device_class = UNIT_DEVICE_CLASS_MAP.get(self._attr_native_unit_of_measurement)
+        self._attr_state_class = UNIT_STATE_CLASS_MAP.get(self._attr_native_unit_of_measurement, SensorStateClass.MEASUREMENT)
+        self._attr_unique_id = f"{entry_id}_{self._uid}_{self._offset}"
+        self._state = None
 
     @property
-    def unique_id(self):
-        """Vrátí jedinečné ID senzoru."""
-        return f"{self._entry_id}_{self._uid}_{self._offset}"
-
-    @property
-    def state(self):
-        """Vrátí aktuální stav senzoru."""
+    def native_value(self):
+        """Vrátí aktuální hodnotu senzoru."""
         return self._state
-
-    @property
-    def unit_of_measurement(self):
-        """Vrátí jednotku měření."""
-        return self._unit_of_measurement
-
-    @property
-    def device_class(self):
-        """Vrátí typ senzoru podle jednotky měření."""
-        return self._device_class
 
     @property
     def device_info(self):
@@ -96,9 +99,11 @@ class SSCPVariableSensor(CoordinatorEntity, SensorEntity):
 
     async def async_update(self):
         """Aktualizuje stav senzoru."""
-        _LOGGER.debug("Updating sensor %s (UID: %s, Offset: %d, Length: %d)", self._name, self._uid, self._offset, self._length)
+        _LOGGER.debug("Updating sensor %s (UID: %s, Offset: %d, Length: %d)", self._attr_name, self._uid, self._offset, self._length)
         try:
             value = self._client.read_variable(self._uid, self._offset, self._length, self._type)
             self._state = value
+            _LOGGER.info("Updated sensor %s with value: %s", self._attr_name, value)
         except Exception as e:
-            _LOGGER.error("Failed to update sensor %s: %s", self._name, e)
+            _LOGGER.error("Failed to update sensor %s: %s", self._attr_name, e)
+            self._state = None
